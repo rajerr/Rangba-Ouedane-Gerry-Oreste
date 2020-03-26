13b92f2f93cfe7141945ddc85eb769b4c7c50023
rajerr236 <rajerr2013@gmail.com>
2020-03-26 15:09:44 +0000

    ajout exo 3 commit 1

--- Exo3/exo3.php
+++ Exo3/exo3.php
@@ -9,24 +9,24 @@
         <form action="" methode="POST" style="margin-left:35%">
             <label style="margin-left:5%">Combien de Mots</label><br><br>
             <input  style=""type="text" name="num" placeholder="donner un nombre" value="<?php if(isset($_POST['num'])){echo $_POST['num'];  } ?>"><br><br>
-            <button style="background-color:skyblue" name="valider" value="valider" type="submit">Valider</button> 
-            <button style="background-color:red" name="annuler" value="annulerr" type="button">Annuler</button> 
+            <button style="background-color:skyblue; height:30px; width: 10%" name="valider" value="valider" type="submit">Valider</button> 
+            <button style="background-color:red; margin-left:1%; height:30px; width: 10%" name="annuler" value="annulerr" type="button">Annuler</button> 
         </form>
 <?php
 if(isset($_POST['valider'])){
-    $num=$_POST['num'];
         if(!(is_numeric($_POST['num']) || ($_POST['num'])<=0) || empty($_POST['num'])){
             echo "veuillez saisir un entier positif";
     }else{
-    //     for($i = 1; $i <=$num ; $i++){
-    //         $strnote = "note" . strval($i+1);
-    //         echo "<input type=\"text\" pattern=\"[0-9]+\"
-    //                 value=\"" . $_POST[$strnote] . "\"
-    //                 placeholder=\"" .($i+1). "\"
-    //                 name=\"" .$strnote. "\"
-    //                 id=\"".$strnote."\" >";
-    //         $notes[$i] = $_POST[$strnote];
-    //   }
+        $num=$_POST['num'];
+        for($i = 1; $i <=$num ; $i++){
+            $strnote = "note" . strval($i+1);
+            echo "<input type=\"text\" pattern=\"[0-9]+\"
+                    value=\"" . $_POST[$strnote] . "\"
+                    placeholder=\"" .($i+1). "\"
+                    name=\"" .$strnote. "\"
+                    id=\"".$strnote."\" >";
+            $notes[$i] = $_POST[$strnote];
+      }
 
     }
 }    
