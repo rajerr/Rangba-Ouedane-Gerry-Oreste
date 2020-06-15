78ffde359091ba29ae46a6894bdef89cccef8d07
rajerr236 <rajerr2013@gmail.com>
2020-06-15 16:36:09 +0000

    Mon projet version a jour Quizz avec BD

--- QUIZZ_DB/pages/accueil.php
+++ QUIZZ_DB/pages/accueil.php
@@ -58,39 +58,45 @@ session_start();
                </div>
             </div> 
 
-            <!--Navbar-->
-        <nav class="navbar navbar-light light-blue lighten-4">
+                    <!--Navbar-->
+            <nav class="navbar navbar-light  purple lighten-4 mb-4 bg-info d-md-none d-lg-none d-xlg-none">
 
-        <!-- Navbar brand -->
-        <a class="navbar-brand" href="#">Navbar</a>
+            <!-- Navbar brand -->
+            <a class="navbar-brand" href="#">Menu</a>
 
-        <!-- Collapse button -->
-        <button class="navbar-toggler toggler-example" type="button" data-toggle="collapse" data-target="#navbarSupportedContent1"
-        aria-controls="navbarSupportedContent1" aria-expanded="false" aria-label="Toggle navigation"><span class="dark-blue-text"><i
-            class="fas fa-bars fa-1x"></i></span></button>
+            <!-- Collapse button -->
+            <button class="navbar-toggler toggler-example purple darken-3" type="button" data-toggle="collapse"
+            data-target="#navbarSupportedContent41" aria-controls="navbarSupportedContent41" aria-expanded="false"
+            aria-label="Toggle navigation"><span class="white-text"><i class="fas fa-bars fa-1x"></i></span></button>
 
-        <!-- Collapsible content -->
-        <div class="collapse navbar-collapse" id="navbarSupportedContent1">
+            <!-- Collapsible content -->
+            <div class="collapse navbar-collapse" id="navbarSupportedContent41">
 
-        <!-- Links -->
-        <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-            <a class="nav-link" href="#">Dashbord <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-            <a class="nav-link" href="#">Features</a>
-            </li>
-            <li class="nav-item">
-            <a class="nav-link" href="#">Pricing</a>
-            </li>
-        </ul>
-        <!-- Links -->
+            <!-- Links -->
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                <a class="nav-link" href="admin.php">Dashbord <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <ul class="navbar-nav">
+                    <a class="nav-link" href="admin.php?lien=creation">Creer Admin</a>
+                    <a class="nav-link" href="admin.php?lien=liste_joueur">Lister Joueurs</a>
+                    </ul>
+                </li>
+                <li class="nav-item">
+                    <ul class="navbar-nav">
+                    <a class="nav-link" href="admin.php?lien=creer_question">Creer Questions</a>
+                    <a class="nav-link" href="admin.php?lien=liste_question">Lister Questions</a>
+                    </ul>
+                </li>
+            </ul>
+            <!-- Links -->
 
-        </div>
-        <!-- Collapsible content -->
+            </div>
+            <!-- Collapsible content -->
 
-        </nav>
-        <!--/.Navbar-->
+            </nav>
+            <!--/.Navbar-->
     </div>
 <footer class="col-12 bg-info fixed-bottom">
     <h6 class="section-heading text-center mx-auto d-block ">COPYRIGHT@ (QUIZZ) 2020</h6>
